8c33b4d62e4d20c5b0e4b9408d7ca7026e6b9f88
alexanderboyuanzhang <46047097+alexanderboyuanzhang@users.noreply.github.com>
2018-12-29 00:27:55 +0100

    loop, function

---
+++ Basics/com/basics/function/LeapYear.py
@@ -0,0 +1,18 @@
+'''
+Created on Dec 29, 2018
+
+@author: Boyuan Zhang
+'''
+
+def is_leap(year):
+    leap = False
+    
+    if(year%400==0):
+        leap = True
+    elif(year%4==0 and year%100!=0):
+        leap = True
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
---
+++ Basics/com/basics/print.py
@@ -0,0 +1,14 @@
+'''
+Created on Dec 29, 2018
+
+@author: Boyuan Zhang 2
+'''
+
+if __name__ == '__main__':
+    n = int(input())
+
+
+i = 1
+while(i<=n):
+    print(i,end='') # print without \n which is so set as by default
+    i=i+1
